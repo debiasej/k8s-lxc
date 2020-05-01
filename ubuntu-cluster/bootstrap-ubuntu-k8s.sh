00f47aac2479e1a762b792ea5933c0f121c15bce
Mario de Biase <debiasej@gmail.com>
2020-05-01 18:35:42 +0000

    Change the color of titles

--- ubuntu-cluster/bootstrap-ubuntu-k8s.sh
+++ ubuntu-cluster/bootstrap-ubuntu-k8s.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+## Aux functions
+title () {
+  printf "\033[34m$*\033[0m\n"
+}
+
 ## Install docker from Docker-ce repository
-echo "[TASK 1] Install docker container engine"
+title "[TASK 1] Install docker container engine"
 # Install Docker CE
 # Set up the repository:
 # Install packages to allow apt to use a repository over HTTPS
@@ -29,30 +34,30 @@ EOF
 mkdir -p /etc/systemd/system/docker.service.d
 
 # Restart  docker service
-echo "[TASK 2] Restart docker service"
+title "[TASK 2] Restart docker service"
 systemctl daemon-reload
 systemctl restart docker
 
 # Add apt repo file for Kubernetes
-echo "[TASK 3] Add apt repo file for kubernetes"
+title "[TASK 3] Add apt repo file for kubernetes"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >> /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
 # Install Kubernetes
-echo "[TASK 4] Install Kubernetes (kubeadm, kubelet and kubectl)"
+title "[TASK 4] Install Kubernetes (kubeadm, kubelet and kubectl)"
 apt-get update && apt-get install -y kubelet=1.18.2-00 kubeadm=1.18.2-00 kubectl=1.18.2-00
 apt-mark hold kubelet kubeadm kubectl
 
 # Start and Enable kubelet service
-echo "[TASK 5] Enable and start kubelet service"
+title "[TASK 5] Enable and start kubelet service"
 systemctl enable kubelet
 echo 'KUBELET_EXTRA_ARGS="--fail-swap-on=false"' > /etc/default/kubelet # Add user-specified flags
 systemctl start kubelet
 
 # Install Openssh server
-echo "[TASK 6] Install and configure ssh"
+title "[TASK 6] Install and configure ssh"
 apt-get install -y ssh
 sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 systemctl enable sshd
@@ -60,11 +65,11 @@ systemctl enable sshd
 systemctl start sshd
 
 # Set Root password
-echo "[TASK 7] Set root password"
+title "[TASK 7] Set root password"
 echo "root:ubuntu" | sudo chpasswd
 
 # Install additional required packages
-echo "[TASK 8] Install additional packages"
+title "[TASK 8] Install additional packages"
 #apt-get install sshpass
 #yum install -y -q which net-tools sudo sshpass less
 
