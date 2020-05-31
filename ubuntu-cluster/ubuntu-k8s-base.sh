16344d03de79681f3ad6af955431d72b2ec20f81
Mario de Biase <debiasej@gmail.com>
2020-05-31 19:32:12 +0200

    Fix title print

--- ubuntu-cluster/ubuntu-k8s-base.sh
+++ ubuntu-cluster/ubuntu-k8s-base.sh
@@ -6,7 +6,7 @@ title () {
 }
 
 ## Install docker from Docker-ce repository
-title "[TASK 1] Install docker container engine"
+printf "\033[1;34m[TASK 1] Install docker container engine\033[0;0m\n"
 # Install Docker CE
 # Set up the repository:
 # Install packages to allow apt to use a repository over HTTPS
@@ -35,31 +35,31 @@ EOF
 mkdir -p /etc/systemd/system/docker.service.d
 
 # Restart  docker service
-title "[TASK 2] Restart docker service"
+printf "\033[1;34m[TASK 2] Restart docker service\033[0;0m\n"
 systemctl daemon-reload
 systemctl restart docker
 
 # Add apt repo file for Kubernetes
-title "[TASK 3] Add apt repo file for kubernetes"
+printf "\033[1;34m[TASK 3] Add apt repo file for kubernetes\033[0;0m\n"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >> /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
 # Install Kubernetes
-title "[TASK 4] Install Kubernetes (kubeadm, kubelet and kubectl)"
+printf "\033[1;34m[TASK 4] Install Kubernetes (kubeadm, kubelet and kubectl)\033[0;0m\n"
 apt-get update && apt-get install -y kubelet=1.18.3-00 kubeadm=1.18.3-00 kubectl=1.18.3-00
 apt-mark hold kubelet kubeadm kubectl
 
 # Install additional required packages
-title "[TASK 5] Install additional packages"
+printf "\033[1;34m[TASK 5] Install additional packages\033[0;0m\n"
 # Update the kernel image
 apt-get install -y linux-image-$(uname -r) sshpass
 # Hack required to provision K8s v1.15+ in LXC containers. The container should be privileged.
 mknod /dev/kmsg c 1 11
 
 # Start and Enable kubelet service
-title "[TASK 6] Enable and start kubelet service"
+printf "\033[1;34m[TASK 6] Enable and start kubelet service\033[0;0m\n"
 systemctl enable kubelet
 # Add user-specified flags
 echo 'KUBELET_EXTRA_ARGS="--cgroup-driver=systemd --runtime-cgroups=/systemd/system.slice --kubelet-cgroups=/systemd/system.slice"' > /etc/default/kubelet
@@ -70,12 +70,12 @@ systemctl daemon-reload
 systemctl restart kubelet
 
 # Install Openssh server
-title "[TASK 7] Install and configure ssh"
+printf "\033[1;34m[TASK 7] Install and configure ssh\033[0;0m\n"
 apt-get install -y ssh
 sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 systemctl enable sshd
 systemctl restart sshd
 
 # Set Root password
-title "[TASK 8] Set root password"
+printf "\033[1;34m[TASK 8] Set root password\033[0;0m\n"
 echo "root:ubuntu" | sudo chpasswd
